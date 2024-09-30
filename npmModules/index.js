be614457aaeef52a4c529d906a433cc30df66f56
itsgauravv <gauravjani3113@gmail.com>
2024-09-30 08:48:19 +0530

    learning event emitter

--- npmModules/index.js
+++ npmModules/index.js
@@ -1,6 +1,18 @@
-const { format } = require("date-fns");
-const { v4: uuid } = require("uuid");
+const logEvents = require("./logEvents");
 
-console.log(format(new Date(), "yyyy/MM/dd\tHH:mm:ss"));
+const EventEmitter = require("events");
 
-console.log(uuid());
+class MyEmitter extends EventEmitter {}
+
+// initialize object
+
+const myEmitter = new MyEmitter();
+
+// add listner for the log event
+
+myEmitter.on("log", (msg) => logEvents(msg));
+
+setTimeout(() => {
+  // Emit event
+  myEmitter.emit("log", "Log Event Emitted");
+}, 2000);
